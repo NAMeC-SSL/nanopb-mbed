18a4f095a0b0e61fb1e72698e0e8913e7594e8c3
Bruno Perraudin <b.perraudin@catie.fr>
2019-06-14 15:44:45 +0200

    Put all includes in a single header file

---
+++ nanopb/nanopb.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2019, CATIE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef CATIE_SIXTRON_NANOPB_H_
+#define CATIE_SIXTRON_NANOPB_H_
+
+#include "pb.h"
+#include "pb_common.h"
+#include "pb_encode.h"
+#include "pb_decode.h"
+
+#endif // CATIE_SIXTRON_NANOPB_H_
